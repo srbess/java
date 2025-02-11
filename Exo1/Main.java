3f2dd179fa7c1fab01d47a984eab9887b8aece88
srbess <bessousirine@gmail.com>
2025-02-11 16:41:18 +0100

    message

---
+++ Exo1/Main.java
@@ -0,0 +1,23 @@
+package Exo1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Voiture voiture = new Voiture();
+
+        voiture.marque = "Toyota";
+        voiture.modele = "Corolla";
+
+        System.out.println("Entrez la couleur de la voiture (Rouge, Vert, Gris) : ");
+        String couleurEntree = scanner.nextLine();
+
+        voiture.demarrer();
+        voiture.freiner();
+        voiture.freiner()
+        
+        scanner.close();
+
+        
+    }
+    
+}
---
+++ Exo1/Voiture.java
@@ -0,0 +1,31 @@
+package Exo1;
+
+import java.util.Scanner;
+
+public class Voiture {
+
+    public String[] couleurAuto={"Rouge", "Vert", "Gris"};
+    public String marque; 
+    public String modele;
+    public String couleur; 
+    public  void demarrer() {
+        System.out.println( "La voiture à démarrer !");   
+    }
+    public  void freiner() {
+        System.out.println( "La voiture à freiner !");   
+    }
+
+    public  void accelerer() {
+        System.out.println( "La voiture à accélerer !");   
+    }
+
+    public boolean estCouleurValide(String couleur) {
+        for (String c : couleurAuto) {
+            if (c.equalsIgnoreCase(couleur)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+}
